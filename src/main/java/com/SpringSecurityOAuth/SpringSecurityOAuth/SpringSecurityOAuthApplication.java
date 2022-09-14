08566d511d84908bc150413c72bd4b41a7a10566
ecvheo1 <ecvheo@naver.com>
2022-09-14 13:15:34 +0900

    :tada: 프로젝트 시작

---
+++ src/main/java/com/SpringSecurityOAuth/SpringSecurityOAuth/SpringSecurityOAuthApplication.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityOAuth.SpringSecurityOAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityOAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringSecurityOAuth/SpringSecurityOAuth/SpringSecurityOAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringSecurityOAuth.SpringSecurityOAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
