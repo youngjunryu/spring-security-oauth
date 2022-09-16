187e1966d696284d2f5ea01ac42821393a015c21
ecvheo1 <ecvheo@naver.com>
2022-09-16 20:53:30 +0900

    :wrench: 프로젝트 설정 불필요한 부분 제거

--- src/main/java/com/SpringSecurityOAuth/SpringSecurityOAuth/SpringSecurityOAuthApplication.java
+++ src/main/java/com/SpringSecurityOAuth/SpringSecurityOAuth/SpringSecurityOAuthApplication.java
@@ -2,8 +2,12 @@ package com.SpringSecurityOAuth.SpringSecurityOAuth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.SpringSecurityOAuth.SpringSecurityOAuth.global.config.AppProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AppProperties.class)
 public class SpringSecurityOAuthApplication {
 
 	public static void main(String[] args) {
